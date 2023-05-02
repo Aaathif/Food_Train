64a9043edb0402a234cf4fa7ee8211be25a574a2
Aathif <aaathif115@gmail.com>
2023-05-03 02:02:36 +0530

    after contact

--- src/components/Profile/Form/FeedbackForm2.js
+++ src/components/Profile/Form/FeedbackForm2.js
@@ -4,64 +4,44 @@ import Form from './Form.module.css'
 import axios from 'axios'
 
 function FeedbackForm() {
-    // const [feedbackType, setFeedbackType] = useState('N/A')
-    // const [category, setCategory] = useState('N/A')
-    // const [description, setDescription] = useState('')
-    const [ data, setData] = useState({feedbackType: "", category: "", description: ""})
-    // const [submittedData, setSubmittedData] = useState([])
+    const [feedbackType, setFeedbackType] = useState('N/A')
+    const [category, setCategory] = useState('N/A')
+    const [description, setDescription] = useState('')
+    const [submittedData, setSubmittedData] = useState([])
 
-    const handleChange = (event) => {
-      const name = event.target.name;
-      const value = event.target.value;
-      setData({...data, [name]: value})
+    const handleType = (event) => {
+        setFeedbackType(event.target.value)
     }
 
-    // const handleType = (event) => {
-    //     setFeedbackType(event.target.value)
-    // }
+    const handleCategory = (event) => {
+        setCategory(event.target.value)
+    }
 
-    // const handleCategory = (event) => {
-    //     setCategory(event.target.value)
-    // }
+    const handleDescription = (event) => {
+        setDescription(event.target.value)
+    }
 
-    // const handleDescription = (event) => {
-    //     setDescription(event.target.value)
-    // }
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const details = {
-          feedbackType: data.feedbackType,
-          category: data.category,
-          description: data.description
+   const handleSubmit = async (event) => {
+     event.preventDefault();
+      const formData = { feedbackType, category, description };
+      console.log("Form data:", formData);
+      // code to submit form data
+      try {
+        const response = await axios.post('http://localhost:8000/api/feedback/', formData);
+        console.log('Response:', response.data);
+        setSubmittedData(response.data);
+      } catch (error) {
+        console.log('Error:', error.message);
       }
-    
-        axios.post('http://localhost:8000/api/contacts', details)
-      .then(response => {
-        console.log(response.details);
-        alert('Successfully Submitted')
-        setData({feedbackType:"N/A", category:"N/A", description:""});
-      })
-      .catch(error => {
-        console.log(error);
-        alert("Something went wrong. Please try again later")
-      })
-
-        // setSubmittedData([...submittedData, data])
-        // setFeedbackType('N/A')
-        // setCategory('N/A')
-        // setDescription('')
-        // alert('Successfully Submitted')
-
-        
     }
+    
 
   return (
     <div className={Form.container}> 
       <h1 className={Form.header}>Feedback Form</h1>
       <form onSubmit={handleSubmit}>
         <label className={Form.label}>Select Type: </label>
-        <select value={data.feedbackType} onChange={handleChange} className={Form.select}>
+        <select value={feedbackType} onChange={handleType} className={Form.select}>
             {options.map((option) => (
                 <option key={option.id} value={option.label}>
                     {option.label}
@@ -70,7 +50,7 @@ function FeedbackForm() {
         </select>
 
         <label className={Form.label}>Select Category: </label>
-        <select value={data.category} onChange={handleChange} className={Form.select}>
+        <select value={category} onChange={handleCategory} className={Form.select}>
             {categories.map((category) => (
                 <option key={category.id} value={category.label}>
                     {category.label}
@@ -81,7 +61,7 @@ function FeedbackForm() {
 
         <label className={Form.label}>Description </label>
         <textarea 
-            value={data.description} onChange={handleChange}
+            value={description} onChange={handleDescription}
             rows={5} cols={50} className={Form.select}
             placeholder='Enter Your Message'
         />
@@ -95,3 +75,76 @@ function FeedbackForm() {
 }
 
 export default FeedbackForm
+
+
+// import React, { useState } from 'react'
+// import {options , categories} from './Data'
+// import Form from './Form.module.css'
+// import axios from 'axios'
+
+// function FeedbackForm() {
+//     // const [feedbackType, setFeedbackType] = useState('N/A')
+//     // const [category, setCategory] = useState('N/A')
+//     // const [description, setDescription] = useState('')
+//     const [ data, setData] = useState({feedbackType: "", category: "", description: ""})
+//     // const [submittedData, setSubmittedData] = useState([])
+
+//     const handleChange = (event) => {
+//       const name = event.target.name;
+//       const value = event.target.value;
+//       setData({...data, [name]: value})
+//     }
+
+//     const handleSubmit = (event) => {
+//         event.preventDefault();
+        
+    
+//         axios.post('http://localhost:8000/api/contacts', data)
+//       .then(response => {
+//         console.log(response.data);
+//         alert('Successfully Submitted')
+//         setData({feedbackType:" ", category:" ", description:" "});
+//       })
+//       .catch(error => {
+//         console.log(error);
+//         alert("Something went wrong. Please try again later")
+//       })
+
+//         // setSubmittedData([...submittedData, data])
+//         // setFeedbackType('N/A')
+//         // setCategory('N/A')
+//         // setDescription('')
+//         // alert('Successfully Submitted')
+
+        
+//     }
+
+//   return (
+//     <div className={Form.container}> 
+//       <h1 className={Form.header}>Feedback Form</h1>
+//       <form onSubmit={handleSubmit}>
+//         <label className={Form.label}>Select Type: </label>
+//         <input type='text' name='feedbackType' onChange={handleChange} value={data.feedbackType} placeholder='Enter Feedback Type'/>
+        
+//         <label className={Form.label}>Select Category: </label>
+//         <input type='text' name='category' onChange={handleChange} value={data.category} placeholder='Enter Category'/>
+        
+//         <label className={Form.label}>Description </label>
+
+//         <textarea onChange={handleChange} rows={5} cols={50} className={Form.select} placeholder='Enter Your Message' value={data.description} />
+//         {/* <textarea 
+//             onChange={handleChange}  
+//             rows={5} cols={50} className={Form.select}
+//             placeholder='Enter Your Message'
+//             value={data.description}
+//         /> */}
+
+
+//         <button type='submit' className={Form.submit}>Submit</button>
+//       </form>
+      
+//     </div>
+//   )
+// }
+
+// export default FeedbackForm
