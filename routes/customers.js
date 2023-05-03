5ef044431497f1301a66a7c9d19dfe12bbcf8442
Aathif <aaathif115@gmail.com>
2023-05-03 10:01:29 +0530

    after c

--- routes/customers.js
+++ routes/customers.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { register, login } from '../controllers/customer.js';
+import { register, login, getCustomer, deleteCustomer } from '../controllers/customer.js';
 
 const router = express.Router();
 
 //Create a new Customer
 router.post('/',register)
 router.post('/login', login)
+router.get('/', getCustomer)
+router.get('/:id', deleteCustomer)
 
 export default router
